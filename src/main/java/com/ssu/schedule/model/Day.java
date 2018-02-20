9bc105135e13fa58512511ea963f0b490c809a4a
vovcyan <vovcyan@yandex.ru>
2018-02-20 19:41:44 +0400

    Fixed issue related to start and end periods of lesson

--- src/main/java/com/ssu/schedule/model/Day.java
+++ src/main/java/com/ssu/schedule/model/Day.java
@@ -1,8 +1,8 @@
 package com.ssu.schedule.model;
 
 public class Day {
-    private String start = "01.09.2016";
-    private String end = "01.07.2017";
+    private String start = "01.09.2018";
+    private String end = "01.07.2050";
     private int weekday;
 
     private int week = 0;
